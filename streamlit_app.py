f980d6511fe1d0debad6df1e063923b6096ad8c3
Ade Olaleye <143139190+Ade-212@users.noreply.github.com>
2026-06-30 13:51:41 +0100

    Reorganize imports and add API request for watermelon
    Moved the import statement for 'requests' to the end of the file and added a request to the Smoothiefroot API for watermelon.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -44,6 +43,7 @@ if ingredients_list:
 
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-        
+
+import requests
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
